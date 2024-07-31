43f169e1803aa9cc0d81f13e284f67aa918f8e3f
daumtto53 <daumtto53@gmail.com>
2024-07-31 22:10:01 +0900

    config(mybatis): mybatis-config set.

---
+++ src/main/java/com/aniDB/aniDB_batch/AniDbBatchApplication.java
@@ -0,0 +1,13 @@
+package com.aniDB.aniDB_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AniDbBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AniDbBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aniDB/aniDB_batch/AniDbBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aniDB.aniDB_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AniDbBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
