d4a1565a492abbaa43035c62df79646bc20ff85b
daumtto53 <daumtto53@gmail.com>
2024-08-12 14:31:29 +0900

    feat(sql): count.sql

---
+++ src/main/resources/sql/count.sql
@@ -0,0 +1,31 @@
+show tables;
+
+SELECT * FROM alternative_publisher_name;
+SELECT COUNT(*) FROM alternative_publisher_name;
+
+SELECT * FROM alternative_title;
+SELECT COUNT(*) FROM alternative_title;
+
+SELECT * FROM anime_adaptation;
+SELECT COUNT(*) FROM anime_adaptation;
+
+SELECT * FROM genre;
+SELECT COUNT(*) FROM genre;
+
+SELECT * FROM publication;
+SELECT COUNT(*) FROM publication;
+
+SELECT * FROM publication_genre;
+SELECT COUNT(*) FROM publication_genre;
+
+SELECT * FROM publication_publisher;
+SELECT COUNT(*) FROM publication_publisher;
+
+SELECT * FROM publisher;
+SELECT COUNT(*) FROM publisher;
+
+SELECT * FROM related_series;
+SELECT COUNT(*) FROM related_series;
+
+SELECT * FROM series_type;
+SELECT COUNT(*) FROM series_type;
